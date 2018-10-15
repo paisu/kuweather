daa20e27b40cb318426e55f24a5d517b42d50053
Minlo <9392001@qq.com>
2018-10-15 23:40:30 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/minlo/kuweather/db/City.java
@@ -0,0 +1,48 @@
+package com.minlo.kuweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * Created by minlo on 2018/10/15
+ *
+ * @link http://www.paisu.net
+ * @description:
+ */
+public class City extends LitePalSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
